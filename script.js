8f89cad8f8f72bf5f10d1db5c70055c1dab0c43e
jessica <jessica4434@gmail.com>
2025-04-18 02:35:46 -0800

    mobile optimization

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-//menu script
-
+//menu for scroll button
 function scrollLeft(button) {
     const scrollSection = button.parentElement.querySelector('.scroll-section');
     scrollSection.scrollBy({ left: -300, behavior: 'smooth' });
@@ -9,3 +8,22 @@ function scrollLeft(button) {
     const scrollSection = button.parentElement.querySelector('.scroll-section');
     scrollSection.scrollBy({ left: 300, behavior: 'smooth' });
   }
+
+  //mobile menu
+  function toggleMenu() {
+    document.querySelector('.main-nav').classList.toggle('active');
+  }
+
+  //Accordion Script
+    const acc = document.querySelectorAll(".accordion");
+    acc.forEach(button => {
+      button.addEventListener("click", function () {
+        this.classList.toggle("active");
+        const panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+          panel.style.display = "none";
+        } else {
+          panel.style.display = "block";
+        }
+      });
+    });
